6b7e24d0d87e776ef7c80a1918906eda85426aa9
Clinton Selke <clinuxrulz@gmail.com>
2025-12-07 12:54:37 +1000

    eliminted memory allocation for head/tails when intrusive linked list is used in Links

--- packages/async-signals-no-past/src/IntrusiveLinkedList.ts
+++ packages/async-signals-no-past/src/IntrusiveLinkedList.ts
@@ -1,3 +1,10 @@
+export interface IntrusiveLinkedListData<Self,A> {
+  head: (self: Self) => A | undefined,
+  setHead: (self: Self, x: A | undefined) => void,
+  tail: (self: Self) => A | undefined,
+  setTail: (self: Self, x: A | undefined) => void,
+}
+
 export interface IntrusiveLinkedListElement<A> {
   prev: (self: A) => A | undefined,
   setPrev: (self: A, x: A | undefined) => void,
@@ -5,58 +12,86 @@ export interface IntrusiveLinkedListElement<A> {
   setNext: (self: A, x: A | undefined) => void,
 }
 
-export class IntrusiveLinkedList<A> {
+export class IntrusiveLinkedList<Self,A> {
+  dataImpl: IntrusiveLinkedListData<Self,A>;
   elementImpl: IntrusiveLinkedListElement<A>;
-  head: A | undefined = undefined;
-  tail: A | undefined = undefined;
 
-  constructor(elementImpl: IntrusiveLinkedListElement<A>) {
+  constructor(
+    dataImpl: IntrusiveLinkedListData<Self,A>,
+    elementImpl: IntrusiveLinkedListElement<A>
+  ) {
+    this.dataImpl = dataImpl;
     this.elementImpl = elementImpl;
   }
 
-  isEmpty(): boolean {
-    return this.head === undefined;
+  isEmpty(self: Self): boolean {
+    return this.dataImpl.head(self) === undefined;
   }
 
-  add(a: A) {
-    if (this.head === undefined) {
-      this.head = this.tail = a;
+  add(self: Self, a: A) {
+    if (this.dataImpl.head(self) === undefined) {
+      this.dataImpl.setHead(self, a);
+      this.dataImpl.setTail(self, a);
       return;
     }
-    this.elementImpl.setNext(this.tail!, a);
-    this.elementImpl.setPrev(a, this.tail);
-    this.tail = a;
+    this.elementImpl.setNext(this.dataImpl.tail(self)!, a);
+    this.elementImpl.setPrev(a, this.dataImpl.tail(self));
+    this.dataImpl.setTail(self, a);
   }
 
-  remove(a: A) {
+  remove(self: Self, a: A) {
     if (this.elementImpl.prev(a) === undefined) {
-      this.head = this.elementImpl.next(this.head!);
-      if (this.head !== undefined) {
-        this.elementImpl.setPrev(this.head, undefined);
+      this.dataImpl.setHead(
+        self,
+        this.elementImpl.next(
+          this.dataImpl.head(self)!
+        ),
+      );
+      if (this.dataImpl.head(self) !== undefined) {
+        this.elementImpl.setPrev(
+          this.dataImpl.head(self)!,
+          undefined,
+        );
       }
     } else {
       this.elementImpl.setNext(this.elementImpl.prev(a)!, this.elementImpl.next(a));
     }
     if (this.elementImpl.next(a) === undefined) {
-      this.tail = this.elementImpl.prev(this.tail!);
-      if (this.tail !== undefined) {
-        this.elementImpl.setNext(this.tail, undefined);
+      this.dataImpl.setTail(
+        self,
+        this.elementImpl.prev(
+          this.dataImpl.tail(self)!
+        ),
+      );
+      if (this.dataImpl.tail(self) !== undefined) {
+        this.elementImpl.setNext(
+          this.dataImpl.tail(self)!,
+          undefined,
+        );
       }
     }
     this.elementImpl.setPrev(a, undefined);
     this.elementImpl.setNext(a, undefined);
   }
 
-  pop(): A | undefined {
-    if (this.head === undefined) {
+  pop(self: Self): A | undefined {
+    if (this.dataImpl.head(self) === undefined) {
       return undefined;
     }
-    let result = this.head;
-    this.head = this.elementImpl.next(this.head);
-    if (this.head === undefined) {
-      this.tail = undefined;
+    let result = this.dataImpl.head(self)!;
+    this.dataImpl.setHead(
+      self,
+      this.elementImpl.next(
+        this.dataImpl.head(self)!
+      ),
+    );
+    if (this.dataImpl.head(self) === undefined) {
+      this.dataImpl.setTail(self, undefined);
     } else {
-      this.elementImpl.setPrev(this.head, undefined);
+      this.elementImpl.setPrev(
+        this.dataImpl.head(self)!,
+        undefined,
+      );
     }
     this.elementImpl.setPrev(result, undefined);
     this.elementImpl.setNext(result, undefined);
