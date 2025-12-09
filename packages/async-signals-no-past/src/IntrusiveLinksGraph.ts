1ed3799acc687416419b447e615e757c0ab63b2c
Clinton Selke <clinuxrulz@gmail.com>
2025-12-09 12:36:55 +1000

    using memory pool in inks graph

--- packages/async-signals-no-past/src/IntrusiveLinksGraph.ts
+++ packages/async-signals-no-past/src/IntrusiveLinksGraph.ts
@@ -1,4 +1,5 @@
 import { IntrusiveLinkedList, IntrusiveLinkedListData, IntrusiveLinkedListElement } from "./IntrusiveLinkedList";
+import { IntrusiveMemoryPool } from "./IntrusiveMemoryPool";
 
 export interface Link<N> {
   dep: N;
@@ -52,13 +53,50 @@ export interface IntrusiveLinksNode<N> {
   readonly subs: IntrusiveLinkedListData<N,Link<N>>;
 }
 
+let linksPoolAnyN = new IntrusiveMemoryPool<Link<any>>(
+  {
+    create() {
+      return {
+        dep: undefined,
+        sub: undefined,
+        prevDep: undefined,
+        nextDep: undefined,
+        prevSub: undefined,
+        nextSub: undefined,
+      };
+    },
+    clean(x) {
+      x.dep = undefined;
+      x.sub = undefined;
+      x.prevDep = undefined;
+      x.nextDep = undefined;
+      x.prevSub = undefined;
+      x.nextSub = undefined;
+    },
+    next(x) {
+      return x.nextSub;
+    },
+    setNext(a, b) {
+      a.nextSub = b;
+    },
+  },
+  1000,
+);
+
+function linksPool<N>(): IntrusiveMemoryPool<Link<N>> {
+  return linksPoolAnyN as unknown as IntrusiveMemoryPool<Link<N>>;
+}
+
 export class IntrusiveLinksGraph<N> {
+  linksPool: IntrusiveMemoryPool<Link<N>>;
   nodeImpl: IntrusiveLinksNode<N>;
   depsImpl: IntrusiveLinkedList<N,Link<N>>;
   subsImpl: IntrusiveLinkedList<N,Link<N>>;
+
   constructor(
     nodeImpl: IntrusiveLinksNode<N>,
   ) {
+    this.linksPool = linksPool<N>();
     this.nodeImpl = nodeImpl;
     this.depsImpl = new IntrusiveLinkedList(
       nodeImpl.deps,
@@ -70,6 +108,13 @@ export class IntrusiveLinksGraph<N> {
     );
   }
 
+  addSub(dep: N, sub: N) {
+    let link = this.linksPool.alloc();
+    link.dep = dep;
+    link.sub = sub;
+    this.addLink(link);
+  }
+
   addLink(link: Link<N>) {
     this.depsImpl.add(link.sub, link);
     this.subsImpl.add(link.dep, link);
@@ -78,6 +123,7 @@ export class IntrusiveLinksGraph<N> {
   removeLink(link: Link<N>) {
     this.depsImpl.remove(link.sub, link);
     this.subsImpl.remove(link.dep, link);
+    this.linksPool.free(link);
   }
 }
 
